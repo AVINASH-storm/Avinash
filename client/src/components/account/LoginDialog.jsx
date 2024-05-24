805681bd335a72acf97348a01dd9a030ab479dce
AVINASH-storm <vangalaavinash50@gmail.com>
2024-05-24 16:39:27 +0530

    first commit

--- client/src/components/account/LoginDialog.jsx
+++ client/src/components/account/LoginDialog.jsx
@@ -7,6 +7,7 @@ import { qrCodeImage } from "../../constants/data";
 import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 import ChatDialog from "../chat/ChatDialog";
+import { addUser } from "../../service/api";
 
 const Component = styled(Box)`
   display: flex;
@@ -52,10 +53,11 @@ const ModifiedList = styled(List)`
 const LoginDialog = () => {
   const { setAccount } = useContext(AccountContext);
 
-  const onLoginSuccess = (res) => {
+  const onLoginSuccess = async (res) => {
     const decoded = jwtDecode(res.credential);
-    console.log(decoded);
     setAccount(decoded);
+
+    await addUser(decoded);
   };
   const onLoginError = (res) => {
     console.log(res);
